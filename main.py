fb1c332dc30541ad63d61e92e8fbc827047eeed9
Aniruddha Jana <aniruddhacr7@gmail.com>
2021-12-30 00:47:09 +0530

    updated colours

--- main.py
+++ main.py
@@ -11,28 +11,31 @@ detector = HandDetector(detectionCon=0.8)
 keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "?"],
         ["A", "S", "D", "F", "G", "H", "J", "K", 'L', ";", "'"],
         ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", " "]]
+buttonList = []
 finalText = ""
 
 keyboard = Controller()
 
 
+##################
 def drawAll(img, buttonlist):
     for button in buttonlist:
         x, y = button.pos
         w, h = button.size
-        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
+        cv2.rectangle(img, button.pos, (x + w, y + h), (47, 47, 47), cv2.FILLED)
         cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
     return img
 
 
 class Button:
-    def __init__(self, pos, text, size=[85, 85]):
+    def __init__(self, pos, text, size=(85, 85)):
         self.pos = pos
         self.text = text
         self.size = size
+##################
 
 
-buttonList = []
+# Drawing Buttons
 for i in range(len(keys)):
     for j, key in enumerate(keys[i]):
         buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
@@ -49,14 +52,15 @@ while True:
             x, y = button.pos
             w, h = button.size
 
+            # Selecting mode
             if x < lmlist[8][0] < x + w and y < lmlist[8][1] < y + h:
-                cv2.rectangle(img, (x - 10, y - 10), (x + w + 5, y + h + 5), (175, 0, 175), cv2.FILLED)
+                cv2.rectangle(img, (x - 10, y - 10), (x + w + 5, y + h + 5), (241, 205, 42), cv2.FILLED)
                 cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
 
-                l, _, _ = detector.findDistance(8, 12, img)
+                length, _, _ = detector.findDistance(8, 12, img)
 
                 # when clicked
-                if l <= 30:
+                if length <= 30:
                     keyboard.press(button.text)
                     cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
                     cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
