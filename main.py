0eccef33dcdcb72e6126db9bb54ff1af16dea624
Aniruddha Jana <aniruddhacr7@gmail.com>
2021-12-30 00:21:42 +0530

    Updated AI MOUSE

--- main.py
+++ main.py
@@ -1,67 +1,68 @@
 import cv2
+import numpy as np
 from cvzone.HandTrackingModule import HandDetector
-from time import sleep
-from pynput.keyboard import Controller
+import time
+import autopy
 
+########
+wCam, hCam = 1280, 720
+frameR = 150  # Frame reduction
+smoothening = 10
+########
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(3, wCam)
+cap.set(4, hCam)
 
-detector = HandDetector(detectionCon=0.8)
-keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "?"],
-        ["A", "S", "D", "F", "G", "H", "J", "K", 'L', ";", "'"],
-        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", " "]]
-finalText = ""
+detector = HandDetector(detectionCon=0.8, maxHands=1)
+wScr, hScr = autopy.screen.size()
+x1, y1 = 0, 0
 
-keyboard = Controller()
-
-
-def drawAll(img, buttonlist):
-    for button in buttonlist:
-        x, y = button.pos
-        w, h = button.size
-        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
-        cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
-    return img
-
-
-class Button:
-    def __init__(self, pos, text, size=[85, 85]):
-        self.pos = pos
-        self.text = text
-        self.size = size
-
-
-buttonList = []
-for i in range(len(keys)):
-    for j, key in enumerate(keys[i]):
-        buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
-
+pTime = 0
+plocX, plocY = 0, 0     # previous location
+clocX, clocY = 0, 0     # current location
 while True:
     success, img = cap.read()
-    img = cv2.flip(img, 1)
     img = detector.findHands(img)
-    lmlist, bboxInfo = detector.findPosition(img)
-    img = drawAll(img, buttonList)
+    lmList, bbox = detector.findPosition(img)
 
-    if lmlist:
-        for button in buttonList:
-            x, y = button.pos
-            w, h = button.size
+    # Tip of index and middle finger
+    if len(lmList) != 0:
+        x1, y1 = lmList[8]
+        x2, y2 = lmList[12]
 
-            if x < lmlist[8][0] < x + w and y < lmlist[8][1] < y + h:
-                cv2.rectangle(img, (x - 10, y - 10), (x + w + 5, y + h + 5), (175, 0, 175), cv2.FILLED)
-                cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+    # Check fingers up
+    fingers = detector.fingersUp()
 
-                l, _, _ = detector.findDistance(8, 12, img)
+    cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (0, 0, 255), 2)
+    # Moving Mode: Only index finger
+    if fingers[1] == 1 and fingers[2] == 0:
 
-                # when clicked
-                if l <= 30:
-                    keyboard.press(button.text)
-                    cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
-                    cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
-                    finalText += button.text
-                    sleep(0.25)
+        x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+        y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+
+        # smoothening
+        clocX = plocX + (x3 - plocX) / smoothening
+        clocY = plocY + (y3 - plocY) / smoothening
+
+        # move cursor
+        autopy.mouse.move(wScr - x3, y3)
+        cv2.circle(img, (x1, y1), 15, (255, 0, 0), cv2.FILLED)
+        plocX, plocY = clocX, clocY
+
+    # Click Mode: Both index and middle finger up
+    if fingers[1] == 1 and fingers[2] == 1:
+        length, img, lineInfo = detector.findDistance(8, 12, img)
+        if length < 40:
+            cv2.circle(img, (lineInfo[4], lineInfo[5]), 10, (0, 255, 0), cv2.FILLED)
+            autopy.mouse.click()
+    # Flip image
+    img = cv2.flip(img, 1)
+
+    # Frame rate
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f"FPS:{int(fps)}", (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
 
     cv2.imshow("Image", img)
     if cv2.waitKey(1) == 27:  # Press ESC to exit
