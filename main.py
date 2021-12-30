96dc6b0d67f6a6f7277986f8c09033bb856b8d36
Aniruddha Jana <aniruddhacr7@gmail.com>
2021-12-30 11:04:42 +0530

    Minor updates

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 from time import sleep
 from pynput.keyboard import Controller
+import time
+import autopy
 
+########
+wCam, hCam = 1280, 720
+frameR = 150  # Frame reduction
+smoothening = 10
+########
 cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
+cap.set(3, wCam)
+cap.set(4, hCam)
 
 detector = HandDetector(detectionCon=0.8)
+wScr, hScr = autopy.screen.size()
+x1, y1 = 0, 0
+pTime = 0
+plocX, plocY = 0, 0     # previous location
+clocX, clocY = 0, 0     # current location
+
 keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "?"],
         ["A", "S", "D", "F", "G", "H", "J", "K", 'L', ";", "'"],
         ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", " "]]
@@ -26,7 +39,7 @@ def drawAll(img, buttonlist):
 
 
 class Button:
-    def __init__(self, pos, text, size=[85, 85]):
+    def __init__(self, pos, text, size=(85, 85)):
         self.pos = pos
         self.text = text
         self.size = size
@@ -53,17 +66,17 @@ while True:
                 cv2.rectangle(img, (x - 10, y - 10), (x + w + 5, y + h + 5), (175, 0, 175), cv2.FILLED)
                 cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
 
-                l, _, _ = detector.findDistance(8, 12, img)
+                length, _, lineInfo = detector.findDistance(8, 12, img)
 
                 # when clicked
-                if l <= 30:
+                if length <= 30:
                     keyboard.press(button.text)
                     cv2.rectangle(img, button.pos, (x + w, y + h), (0, 255, 0), cv2.FILLED)
                     cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
                     finalText += button.text
                     sleep(0.25)
 
-    cv2.imshow("Image", img)
+    cv2.imshow("Screen", img)
     if cv2.waitKey(1) == 27:  # Press ESC to exit
         break
 
